445237b9e42607ead60b379d379efde4b86064f8
Jerry Velazquez <Jvr572@gmail.com>
2024-04-16 13:29:35 -0400

    have to go over the itertools i used here

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,8 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+from itertools import tee, zip_longest, chain
+
+def pairwise_offset(sequence, fillvalue="*", offset=1):
+    it1, it2 = tee(sequence,2)
+    print(it1,it2)
+    return zip_longest(it1,chain(fillvalue * offset, it2), fillvalue=fillvalue)
+    
+pairwise_offset(['a','b','c'], '*',  1)
